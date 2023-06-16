6ef9a6cca616ab76a88cbc1d9cb38c96da57a243
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 15:49:48 -0700

    syntax fixed1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %n byte(s)", sizeof(char));
-printf("Size of an int: %n byte(s)", sizeof(int));
-printf("Size of a long int: %n byte(s)", sizeof(long int));
-printf("Size of a long long int: %n byte(s)", sizeof(long long int));
-printf("Size of a float: %n byte(s)", sizeof(float));
+printf("Size of a char: %zu byte(s)", sizeof(char));
+printf("Size of an int: %zu byte(s)", sizeof(int));
+printf("Size of a long int: %zu byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+printf("Size of a float: %zu byte(s)", sizeof(float));
 return (0);
 }
