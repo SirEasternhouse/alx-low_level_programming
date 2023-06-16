ffaae2d7091882ba29c53d2ad894e83bb2753a8a
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 06:54:47 -0700

    2nd att 4-puts.c3.4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+int main(void)
+
 /*
  *  puts function programme
  */
 
-int main(void)
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
-	puts (message);
+	puts(message);
 	return(0);
+
 }
