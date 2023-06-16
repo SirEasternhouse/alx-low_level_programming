ea6f6eafa56d8c1f552f8bf80c7623d888b454bf
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 05:54:32 -0700

    2nd att 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
+/* inclusion of library header */
+
 #include <stdio.h>
+
+/*
+ *  puts function programme
+ */
 int main(void)
-{const char *message = "\"Programming is like building a multilingual puzzle\"" ;
- puts(message);
- return(0);
+{
+	const char *message = "Programming is like building a multilingual puzzle;
+ 	puts(message);
+ 	return(0);
 }
