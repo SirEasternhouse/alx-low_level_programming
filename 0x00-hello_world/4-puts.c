5e3b349b0843b4026888903ea56fba38b4f57389
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-15 17:47:06 -0700

    puts c 2.5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
-{ const char *message="\"""Programming is like building a multilingual puzzle""\"";
+{ const char *message="\"Programming is like building a multilingual puzzle\"";
   puts(message);
   return 0;
 }
