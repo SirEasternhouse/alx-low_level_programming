4fd6e9c15679b61cf8d51e26ff179408308fc803
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 11:31:38 -0700

    betty fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-int main(void)
-
-/*
+/**
  *  puts function programme
  */
-
+int main(void)
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
 	puts(message);
