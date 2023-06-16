d49fa707032d32693706174b6c98a8c1f2f6f5b4
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 12:15:21 -0700

    betty fix 2.32

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,6 @@
  */
 int main(void) /** main is the startt of the function */
 {const char *message = "\"Programming is like building a multilingual puzzle";
-	
 puts(message);
 return(0);/* return of0 indicates successful execution of programe */
 }
