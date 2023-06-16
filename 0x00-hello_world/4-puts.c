4b8de3e6d71eb4eb17014d01e43a2e0e66cd4070
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 12:22:56 -0700

    betty fix 2.33

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
-/* Hello,puts */
 #include <stdio.h>
 
-/**
- *  The main function is the entry point of the programme.
- *  It prints a message using the put function.
- *
- *  return 0  shows successful execution.
- */
-int main(void) /** main is the startt of the function */
+/* The main function is the entry point of the programme.*/
+int main(void) /* main is the startt of the function */
 {const char *message = "\"Programming is like building a multilingual puzzle";
 puts(message);
 return(0);/* return of0 indicates successful execution of programe */
