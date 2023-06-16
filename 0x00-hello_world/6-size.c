145567b71c476f1d3fe23847cc2b6f5ab9511b80
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 15:26:02 -0700

    size of task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - the file size of a characther
+ *Return: the file sile of characters
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)"sizeof(char));
+printf("Size of an int: %zu byte(s)"sizeof(int));
+printf("Size of a long int: %zu byte(s)"sizeof(long int));
+printf("Size of a long long int: %zu byte(s)"sizeof(long long int));
+printf("Size of a float: %zu byte(s)"sizeof(float));
+return (0);
+}
