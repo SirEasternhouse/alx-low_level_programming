2bc0edd07c71ec9716b6a1a7ef7b1ffc239e4fa7
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-16 11:51:18 -0700

    betty fix 2.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,12 +6,11 @@ int main(void)
  *  The main function is the entry point of the programme.
  *  It prints a message using the put function.
  *
- *  @return 0 it shows successful execution.
+ *  return 0  shows successful execution.
  */
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
 	
-	puts (message);
+	puts( message);
 	return(0);
-
 }
