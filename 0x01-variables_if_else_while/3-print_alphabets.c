10db0d5d2f376fee2f36daceaa9db1fdb81075a5
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-18 12:49:20 -0700

    Task 3 alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets
+ * Return: value of 0 indicated success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
