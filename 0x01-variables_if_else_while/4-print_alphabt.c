ed8ba9fe4619b3d8f3960d388a248b49c914421d
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-18 16:31:35 -0700

    task 4: alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet excluding char
+ * Result:0 show succesful execution
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
