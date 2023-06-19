650381ad684ee9a2a160172a114e438ef474e671
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-19 13:33:01 -0700

    adv task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * main - string output
+ *
+ * Return: 1 indicate succes
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
