bce7806493faa23944746bcdd804d7ae2e53dda2
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-18 17:11:02 -0700

    Task 5: numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - numbers from 0-9 will be printed
+ *
+ * Return: 0 indicated sucessful execution
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while(i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
