b9caa5cdd7d86f6a79fc037b5f066308ccff28a0
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-20 12:06:23 -0700

    tskv0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,22 @@
 int main(void)
 {
 	char c = '_';
-	
-	while (c)
-	{
+	char d = 'p';
+	char e = 'u';
+	char f = 't';
+	char g = 'c';
+	char h = 'h';
+	char i = 'a';
+	char j = 'r';
+
 	_putchar(c);
-	}
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar(i);
+	_putchar(j);
 	_putchar('\n');
 	return (0);
 }
