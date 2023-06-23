b891a5ad0625dec7c906c09d9a175d854e459ffd
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-23 16:10:09 -0700

    task 2 :mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * mul - multiply 2 numbers
+ *@a: The character to input
+ *@b: the character to input
+ *@result: the result from a*b
+ *
+ * Return: 0 indicate success
+ */
+
+int mul(int a, int b)
+{
+	int i = a;
+	int j = b;
+	int result = i * j;
+
+	return (result);
+}
