7d4e1c6fc8af528cc8ea221887a86fc41830040d
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-23 16:44:52 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *print_most_numbers - print numbers from 0-9 excluding 2 & 4
+ *
+ * Return: 0 indicate success
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for  (i =0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
