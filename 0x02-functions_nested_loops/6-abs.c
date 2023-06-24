a20b53fa59fab5a7738459cef3a3af78aefe40f1
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-24 05:36:43 -0700

    asolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *_abs - compute the abs value of integer
+ *@n: The character to print
+ *
+ * Return: success =1 & -1
+ */
+
+int _abs(int i)
+{
+	if ( i < 0)
+	{
+		i= -i;
+	} 
+	return (i);
+}
