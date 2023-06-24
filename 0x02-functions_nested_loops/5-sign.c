916e0baee443731cd522a5c78f7d2dd24190631c
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-24 05:00:21 -0700

    fixed sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,26 +10,19 @@
 
 int print_sign(int n)
 {
-	while (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
-	}
-	while (n < 0)
-	{
-		_putchar('-');
-	}
-	while(n == 0)
-	{
-		_putchar('0');
-	}
-
-	if (n == '+')
-	{
 		return (1);
 	}
-	if (n == '-')
+	else if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
 	}
 	return (0);
 
