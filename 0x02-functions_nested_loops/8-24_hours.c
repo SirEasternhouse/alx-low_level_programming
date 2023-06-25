37d1d0d61ad937308dd2d961987db4adfd2e556a
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-06-25 06:00:02 -0700

    24 hours0.1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,14 +5,14 @@
  *jack_bauer - compute the abs value of integer
  *
  *
- * Return: success =1 & -1
+ * Return: printed time
  */
 
 void jack_bauer(void)
 {
 	int  i, j, k, l;
-	
-	for (i =0; i < 3; i++)
+
+	for (i = 0; i < 3; i++)
 	{
 		j = 0;
 		while (j < 10)
@@ -21,10 +21,6 @@ void jack_bauer(void)
 			{
 				break;
 			}
-			/*_putchar('0' + i);
-			_putchar('0' + j);
-			_putchar('\n');*/
-			
 			for (k = 0; k < 6; k++)
 			{
 				l = 0;
