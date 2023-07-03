c08cb174529013a7d8b6b9a525d368d67a27fdf8
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-03 15:53:11 -0700

    pointer at 1st instance

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strchr - find 1st instance of character
+ *@s: string to look for 1st instance character
+ *@c: character to look for
+ *
+ *Return: pointer at 1st instance of char or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ("NULL");
+}
