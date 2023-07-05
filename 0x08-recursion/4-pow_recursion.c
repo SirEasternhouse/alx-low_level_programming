3b9aa79c03748fbfd93419746b925417b94e8ce8
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-05 10:20:54 -0700

    exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_pow_recursion - 
+ *@x:
+ *@y:
+ *
+ *Return: value to the power y
+ * 
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y -1));
+	}
+}
