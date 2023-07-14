adaaa357d4623fc41811eb5f5f96aae8412bfde1
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-14 10:25:17 -0700

    2d memory allocation

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *alloc_grid - create a 2d array
+ *@width: width of the array
+ *@height: height of the array
+ *
+ *Return: 2D array initialized with 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid = malloc(width * sizeof(int *));
+
+	if (grid != NULL)
+	{
+		int i = 0;
+
+		while (i < width)
+		{
+			grid[i] = malloc(height * sizeof(int));
+
+			if (grid[i] != NULL)
+			{
+				int j = 0;
+
+				while (j < height)
+				{
+					grid[i][j] = 0;
+					j++;
+				}
+			}
+			else
+			{
+				int k = 0;
+
+				while (k < i)
+				{
+					free(grid[k]);
+					k++;
+				}
+				free(grid);
+				return (NULL);
+			}
+			i++;
+		}
+	}
+	return (grid);
+}
