8976ec266dce03e9c8124a003fee759893dca182
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-14 12:20:43 -0700

    updated calloc.2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -46,14 +46,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	else
 	{
-		if (nmemb == 0)
-		{
-			return (ptr);
-		}
-		else
-		{
-			return (NULL);
-		}
+		return (NULL);
 	}
 	return (ptr);
 }
