52f397b2a20e27468f94969ed3eb6093a5459a3a
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-17 16:23:47 -0700

    dog tag 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef _DOG_H
 #define _DOG_H
-
+/**
+ *struct dog - defining the of tag datatype
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *
+ *Description: dog tag to make dog detail fetching convenient
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog;
 #endif
