94fe481977516639b1837a6b1762fdd61c387435
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-28 13:08:53 -0700

    fixed header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
- * struct my_struct - Short description
- * @a: First member
- * @b: Second member
- * @c: Third member
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: Longer description
+ * Description: singly linked list node structure
  */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-/* function prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
