24bd6aa140427fd7904ee3299ec2e5d54b0b654e
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-08-01 05:05:35 -0700

    fix output format

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,13 +14,12 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d ", h->n);
+		printf("%d\n", h->n);
 
 		h = h->next;
 
 		node_count++;
 	}
-	printf("\n");
 
 	return (node_count);
 }
