7ee9caeecf249eadeb8cad7d2c7e12e9bfb3f2e9
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-08-07 17:31:11 -0700

    create file fix2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,17 +5,6 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
  *print_error - print an error message
  *@message: message to print
  *
@@ -29,10 +18,10 @@ void print_error(const char *message)
 
 	while (*c != '\0')
 	{
-		_putchar(*c);
+		putchar(*c);
 		c++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
 /**
