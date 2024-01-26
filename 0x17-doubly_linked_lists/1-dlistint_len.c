7c8936e2853d3bd395b280e6332f1d3018762642
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-26 03:46:03 -0800

    length of node

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ *dlistint_len - return the number of elements in a doubly linked list
+ *@h: head of node
+ *
+ *Return: number of elements in a node
+ */
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
