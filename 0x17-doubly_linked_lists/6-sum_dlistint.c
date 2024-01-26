5e7411dd4058188e6378636f1433d6ce9e715231
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-26 10:13:17 -0800

    sum of linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ *sum_dlisint - sum of a doubly linked list
+ *@head: head of node
+ *
+ *
+ *Return: sum of doubly linked list
+ */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
