08b36767165153bff43370a675af079f8d5dd9a4
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-26 05:36:36 -0800

    free linked list double

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+/**
+ * free_dlistint - free a doubly linked list
+ *
+ *
+ * Retrun: none
+ */
 
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *current = head;
+	dlistint_t *next;
+
+	while ((current) != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
 }
