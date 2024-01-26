a533899102bb9a5cf7ceb4c1002b8244d4661820
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-01-26 10:03:27 -0800

    node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stddef.h>
+/**
+ *get_dnodeint_at_index - get node data at index
+ *@head: head of node
+ *@index: index of node
+ *
+ *Return: data at index
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *current = head;
+	unsigned int i = 0;
+
+	if (head == NULL || (int)index < 0)
+	{
+		return (NULL);
+	}
+
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	return (current);
 }
