8207e04bbe17a8362c634f5f2ddf28791dc3f77f
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-21 09:43:14 -0800

    finding a key in a hash table

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 /**
+ *key_index - Computes the index of a key in the hash table's array
+ *@key: The key
+ *@size: the size of the array
  *
- *
- *
- *
- *
+ *Return: The index where the key should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	return (hash_djb2(key) % size);
 }
