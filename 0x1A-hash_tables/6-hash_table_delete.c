7b37a59bc614ed149f59c2f70e63794635d32d75
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2024-02-21 13:54:34 -0800

    delete # table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,33 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 /**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
  *
- *
- *
- *
- *
+ *Return: none
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *node, *temp;
+	/* Check if the hash table is NULL */
+	if (ht == NULL)
+		return;
+	/* Iterate through each element of the array */
+	for (i = 0; i < ht->size; i++)
+	{
+		/* Free the linked list at each index */
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+	/* Free the array and the hash table itself */
+	free(ht->array);
+	free(ht);
 }
